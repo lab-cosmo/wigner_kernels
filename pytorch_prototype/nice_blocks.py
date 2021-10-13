bff44a9bd265b2d5b3b2e519d4c7c61a711ba95e
serfg <sergei.pozdnyakov@gmail.com>
2021-10-13 15:55:16 +0200

    invariant branch

--- pytorch_prototype/nice_blocks.py
+++ pytorch_prototype/nice_blocks.py
@@ -166,15 +166,15 @@ def apply_indices(covariants, indices):
     return result
 
 class Expansioner(torch.nn.Module):
-    def __init__(self, lambda_max):
+    def __init__(self, lambda_max, num_expand):
         super(Expansioner, self).__init__()
         self.lambda_max = lambda_max
+        self.num_expand = num_expand
         
     def fit(self, first_even,
             first_odd,
             second_even,
             second_odd,
-            num_expand = None, 
             clebsch = None):
         
         all_keys = list(first_even.keys()) + list(first_odd.keys()) + \
@@ -188,7 +188,7 @@ class Expansioner(torch.nn.Module):
         else:
             self.clebsch = clebsch
             
-        if num_expand is not None:
+        if self.num_expand is not None:
             first_even_idx = get_sorting_indices(first_even)
             first_odd_idx = get_sorting_indices(first_odd)
             second_even_idx = get_sorting_indices(second_even)
@@ -200,7 +200,7 @@ class Expansioner(torch.nn.Module):
             second_odd = apply_indices(second_odd, second_odd_idx)
           
             task_even_even, task_odd_odd, task_even_odd, task_odd_even = \
-                get_thresholded_tasks(first_even, first_odd, second_even, second_odd, num_expand,
+                get_thresholded_tasks(first_even, first_odd, second_even, second_odd, self.num_expand,
                                       self.l_max, self.lambda_max)
             
             
@@ -289,14 +289,48 @@ class BodyOrderIteration(torch.nn.Module):
             res_even, res_odd = self.compressor(res_even, res_odd)
         
         return res_even, res_odd
-            
-        
+
+def filter_invariants(covs):
+    if '0' in covs.keys():
+        return covs['0'][:, :, 0]
+    else:
+        return None
 
 class NICE(torch.nn.Module):
+    
+    @staticmethod
+    def convert_block(block):
+        if isinstance(block, list):
+            cov_block = block[0]
+            inv_block = block[1]
+        else:
+            cov_block = block
+            inv_block = None
+        return cov_block, inv_block
+    
+    @staticmethod
+    def is_empty(block):
+        cov_block, inv_block = NICE.convert_block(block)
+        return (inv_block is None) and (cov_block is None)
+    
+    @staticmethod
+    def computes_covariants(block):
+        cov_block, inv_block = NICE.convert_block(block)
+        return cov_block is not None
+    
     def __init__(self, blocks, initial_compressor):
         super(NICE, self).__init__()
+        
         self.blocks = blocks
         self.initial_compressor = initial_compressor
+        for block in self.blocks:
+            if NICE.is_empty(block):
+                raise ValueError("fully empty blocks should not present")
+        
+        for i, block in enumerate(self.blocks):
+            if (not NICE.computes_covariants(block)) and (i + 1 < len(self.blocks)):
+                raise ValueError("all inner blocks should compute covariants (should have covariant branch)")
+                
         
         
     def fit(self, even_initial, odd_initial):
@@ -311,14 +345,24 @@ class NICE(torch.nn.Module):
         odd_old = [odd_initial]
         
         for block in self.blocks:
-            block.fit(even_now, odd_now, even_initial, odd_initial,
+            cov_block, inv_block = NICE.convert_block(block)
+            if cov_block is not None:
+                cov_block.fit(even_now, odd_now, even_initial, odd_initial,
                       even_old, odd_old)
-            even_now, odd_now = block(even_now, odd_now, even_initial, odd_initial,
-                                      even_old, odd_old)
+            if inv_block is not None:
+                inv_block.fit(even_now, odd_now, even_initial, odd_initial,
+                      even_old, odd_old)
+                
+            if cov_block is not None:
+                even_now, odd_now = cov_block(even_now, odd_now, even_initial, odd_initial,
+                                          even_old, odd_old)
+            else:
+                even_now, odd_now = None, None
+            
             even_old.append(even_now)
             odd_old.append(odd_now)
             
-    def forward(self, even_initial, odd_initial):
+    def forward(self, even_initial, odd_initial, return_covariants = False):
         if self.initial_compressor is not None:
             even_initial, odd_initial  = self.initial_compressor(even_initial, odd_initial)
         
@@ -328,10 +372,45 @@ class NICE(torch.nn.Module):
         even_old = [even_initial]
         odd_old = [odd_initial]
         
-        for block in self.blocks:
-            even_now, odd_now = block(even_now, odd_now, even_initial, odd_initial,
-                                      even_old, odd_old)
+        body_order_now = 1
+        even_invs = {str(body_order_now) : filter_invariants(even_initial)}
+        odd_invs = {str(body_order_now) : filter_invariants(odd_initial)}
+      
+        
+        for current in self.blocks:
+            body_order_now += 1
+            cov_block, inv_block = NICE.convert_block(current)
+            
+            if inv_block is not None:
+                even_inv_now, odd_inv_now = inv_block(even_now, odd_now, even_initial, odd_initial,
+                                          even_old, odd_old)
+                even_invs[body_order_now] = filter_invariants(even_inv_now)
+                odd_invs[body_order_now] = filter_invariants(odd_inv_now)
+            else:
+                even_invs[body_order_now] = filter_invariants(even_now)
+                odd_invs[body_order_now] = filter_invariants(odd_now)
+                
+            
+            if cov_block is not None:
+                even_now, odd_now = cov_block(even_now, odd_now, even_initial, odd_initial,
+                                          even_old, odd_old)
+            else:
+                even_now, odd_now = None, None
+                
+            
+                
             even_old.append(even_now)
             odd_old.append(odd_now)
-        return even_old, odd_old
+            
+        if return_covariants:
+            even_dict, odd_dict = {}, {}
+            for body_order in range(len(even_old)):
+                even_dict[str(body_order + 1)] = even_old[body_order]
+            
+            for body_order in range(len(odd_old)):
+                odd_dict[str(body_order + 1)] = odd_old[body_order]
+                
+            return even_invs, odd_invs, even_dict, odd_dict
+        else:
+            return even_invs, odd_invs
        
