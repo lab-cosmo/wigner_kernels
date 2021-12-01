9a4fbebce8544fd62c0228fae140fa2203198691
sergey <sergei.pozdnyakov@gmail.com>
2021-12-01 19:04:57 +0100

    cleaning

--- pytorch_prototype/test.pyx
+++
@@ -1,7 +0,0 @@
-cimport cython
-import cython
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-cpdef int multiply(int a, int b):
-    return a * b
--- pytorch_prototype/test_cython.py
+++
@@ -1,4 +0,0 @@
-import pytorch_prototype.test
-
-def verify(a, b):
-    return pytorch_prototype.test.multiply(a, b)
