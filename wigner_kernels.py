3867adffa1d5aae2134bf50cb1cd2707ac5d75c1
frostedoyster <bigi.f@libero.it>
2022-09-14 11:38:28 +0200

    Batching kernel initialization (on CPU) as well
    due to memory issues.

--- wigner_kernels.py
+++ wigner_kernels.py
@@ -1,6 +1,5 @@
 import torch
 import copy
-# torch.set_default_dtype(torch.float64)
 import numpy as np
 import ase.io
 
@@ -8,46 +7,46 @@ from pytorch_prototype.code_pytorch import *
 from pytorch_prototype.utilities import *
 from pytorch_prototype.clebsch_gordan import ClebschGordan
 
-from equistore import Labels, TensorBlock, TensorMap
+from equistore import TensorBlock, TensorMap
 from rascaline import SphericalExpansion
 
-
+# torch.set_default_dtype(torch.float64)
+# torch.manual_seed(0)
 
 METHANE_PATH = 'methane.extxyz'
 HARTREE_TO_EV = 27.211386245988
 HARTREE_TO_KCALMOL = 627.5
 
-train_slice = '0:500'
-validation_slice = '500:600'
-test_slice = '600:700'
+n_test = 1000
+n_train = 5000
+n_validation = int(n_train/10)
 
-L_MAX = 4
+test_slice = str(0) + ":" + str(n_test)
+train_slice = str(n_test) + ":" + str(n_test+n_train)
+validation_slice = str(n_test+n_train) + ":" + str(n_test+n_train+n_validation)
+
+L_MAX = 3
 clebsch = ClebschGordan(L_MAX)
 
 hypers_spherical_expansion = {
-    "cutoff": 6.3,
-    "max_radial": 20,
+    "cutoff": 6.5,
+    "max_radial": 22,
     "max_angular": L_MAX,
     "atomic_gaussian_width": 0.2,
-    "center_atom_weight": 1.0,
+    "center_atom_weight": 0.0,
     "radial_basis": {
         "Gto": {},
     },
     "cutoff_function": {
-        "ShiftedCosine": {"width": 0.5},
+        "Step": {},
     },
-    "radial_scaling":  {"Willatt2018": { "scale": 2.0, "rate": 0.8, "exponent": 2}},
+    # "radial_scaling":  {"Willatt2018": { "scale": 2.0, "rate": 0.8, "exponent": 2}},
 }
 
-BATCH_SIZE = 2000
+BATCH_SIZE = 10000
 DEVICE = 'cuda'
 
-
 calculator = SphericalExpansion(**hypers_spherical_expansion)
-
-structures = ase.io.read(METHANE_PATH, index = train_slice)
-
-
 
 def initialize_wigner_single_l(first, second):
     first_b_size, first_m_size = first.shape[0], first.shape[1]
@@ -58,13 +57,15 @@ def initialize_wigner_single_l(first, second):
     result = result.reshape(first_b_size, first_m_size, second_b_size, second_m_size)
     return result.transpose(1, 2)
 
-def initialize_wigner_single_species(first, second, center_species):
+def initialize_wigner_single_species_batched(first, second, center_species, idx_1, idx_2):
+    idx_1_begin, idx_1_end = idx_1
+    idx_2_begin, idx_2_end = idx_2
     lmax = np.max(first.keys["spherical_harmonics_l"])
     result = {}
     for l in range(lmax+1):
         result[str(l) + "_" + str(1)] = initialize_wigner_single_l(
-                first.block(spherical_harmonics_l=l, species_center=center_species).values, 
-                second.block(spherical_harmonics_l=l, species_center=center_species).values
+                first.block(spherical_harmonics_l=l, species_center=center_species).values[idx_1_begin:idx_1_end, :, :], 
+                second.block(spherical_harmonics_l=l, species_center=center_species).values[idx_2_begin:idx_2_end, :, :]
                 )
     return result
 
@@ -75,6 +76,10 @@ class WignerKernel(torch.nn.Module):
                 for _ in range(num_iterations)]
         self.main = nn.ModuleList(main)
         self.last = WignerCombiningUnrolled(clebsch.precomputed_, 0, algorithm = 'vectorized')
+    
+    #############################
+    # Need to try what happens when combining e.g. nu = 2 and nu = 2 kernels
+    #############################
             
     def forward(self, X):
         result = []
@@ -93,33 +98,45 @@ def compute_kernel(model, first, second, batch_size = 1000, device = 'cpu'):
 
     n_first = len(np.unique(first.block(0).samples["structure"]))
     n_second = len(np.unique(second.block(0).samples["structure"]))
-    wigner_invariants = torch.zeros((n_first, n_second, 4))  # Hardcoded last dimension 
+    nu_max = len(model.main) + 2  # nu = 1 and last iteration not counted in model.main
+    wigner_invariants = torch.zeros((n_first, n_second, nu_max))
+    batch_size_each = int(np.sqrt(batch_size))  # A batch size for each of the two tensor maps involved.
   
     for center_species in all_species:
-        wigner_c = initialize_wigner_single_species(first, second, center_species)
-        
+        # if center_species == 1: continue  # UNCOMMENT FOR C-ONLY VERSION
         structures_first = first.block(spherical_harmonics_l=0, species_center=center_species).samples["structure"]
         structures_second = second.block(spherical_harmonics_l=0, species_center=center_species).samples["structure"]
-        places = []
-        for structure_1 in structures_first:
-            for structure_2 in structures_second:
-               places.append((structure_1, structure_2)) 
+        len_first = structures_first.shape[0]
+        len_second = structures_second.shape[0]
 
-        for key in wigner_c.keys():
-            initial_shape = [wigner_c[key].shape[0], wigner_c[key].shape[1]]
-            wigner_c[key] = wigner_c[key].reshape([-1, wigner_c[key].shape[2], wigner_c[key].shape[3]])
-        
-        total = initial_shape[0] * initial_shape[1]
-        result = []
-        #print(total, batch_size)
-        #print(initial_shape)
-        for ind in tqdm.tqdm(range(0, total, batch_size)):
-            now = {}
-            for key in wigner_c.keys():
-                now[key] = wigner_c[key][ind : ind + batch_size].to(device)
-            result_now = model(now).to('cpu')
-            for i, place in enumerate(places[ind : ind + batch_size]):
-                wigner_invariants[place] += result_now[i]
+        # Batched calculation, starting from nu = 1 kernel initialization:
+        for idx_1_begin in tqdm.tqdm(range(0, len_first, batch_size_each)):
+            idx_1_end = min(idx_1_begin+batch_size_each, len_first)
+            dimension_1 = idx_1_end - idx_1_begin
+
+            for idx_2_begin in range(0, len_second, batch_size_each):
+                idx_2_end = min(idx_2_begin+batch_size_each, len_second)
+                dimension_2 = idx_2_end - idx_2_begin
+
+                wigner_c = initialize_wigner_single_species_batched(
+                    first,
+                    second, 
+                    center_species,
+                    (idx_1_begin, idx_1_end),
+                    (idx_2_begin, idx_2_end),
+                    )            
+
+                for key in wigner_c.keys():
+                    wigner_c[key] = wigner_c[key].reshape([dimension_1*dimension_2, wigner_c[key].shape[2], wigner_c[key].shape[3]])               
+                now = {}
+                for key in wigner_c.keys():
+                    now[key] = wigner_c[key].to(device)
+                result_now = model(now).to('cpu')
+                result_now = result_now.reshape([dimension_1, dimension_2, nu_max])
+
+                for i_1 in range(idx_1_begin, idx_1_end):
+                    for i_2 in range(idx_2_begin, idx_2_end):
+                        wigner_invariants[structures_first[i_1], structures_second[i_2]] += result_now[i_1-idx_1_begin, i_2-idx_2_begin]
 
     return wigner_invariants
 
@@ -142,6 +159,8 @@ def move_to_torch(rust_map: TensorMap) -> TensorMap:
             blocks = torch_blocks
             )
 
+print("Calculating expansion coefficients", flush = True)
+
 train_coefs = calculator.compute(train_structures)
 train_coefs.keys_to_properties("species_neighbor")
 train_coefs = move_to_torch(train_coefs)
@@ -153,6 +172,8 @@ validation_coefs = move_to_torch(validation_coefs)
 test_coefs = calculator.compute(test_structures)
 test_coefs.keys_to_properties("species_neighbor")
 test_coefs = move_to_torch(test_coefs)
+
+print("Expansion coefficients done", flush = True)
 
 '''
 L2_mean = get_L2_mean(train_coefs)
@@ -176,15 +197,6 @@ train_test_kernel = train_test_kernel.data.cpu()
 
 for i in range(10):
     print(train_train_kernel[i, i])
-
-'''
-print(train_train_kernel.shape)
-print(train_validation_kernel.shape)
-print(train_test_kernel.shape)
-train_train_kernel = train_train_kernel[:, :, -1]
-train_validation_kernel = train_validation_kernel[:, :, -1]
-train_test_kernel = train_test_kernel[:, :, -1]
-'''
 
 def get_rmse(first, second):
     return torch.sqrt(torch.mean((first - second)**2))
@@ -226,7 +238,7 @@ class ValidationCycle(torch.nn.Module):
 
         # Kernel regularization:
         self.sigma_exponent = torch.nn.Parameter(
-            torch.tensor([-8], dtype = torch.get_default_dtype())
+            torch.tensor([-6], dtype = torch.get_default_dtype())
             )
 
         # Coefficients for mixing kernels of different body-orders:
@@ -235,7 +247,6 @@ class ValidationCycle(torch.nn.Module):
 
     def forward(self, K_train, y_train, K_val):
         coefficients = torch.cat([self.coefficients, torch.ones((1,))], dim = -1)
-        # sigma = self.sigma
         sigma = torch.exp(self.sigma_exponent)
         n_train = K_train.shape[0] 
         n_val = K_val.shape[1]
@@ -256,7 +267,7 @@ optimizer = torch.optim.Adam(validation_cycle.parameters(), lr = 1e-2)
 
 print("Beginning hyperparameter optimization")
 best_rmse = 1e20
-for i in range(1000):
+for i in range(10000):
     optimizer.zero_grad()
     validation_predictions = validation_cycle(train_train_kernel, train_energies, train_validation_kernel)
 
@@ -271,40 +282,18 @@ for i in range(1000):
     validation_loss.backward()
     optimizer.step()
 
-    if i % 100 == 0:
-        print(best_rmse, best_nu_zero_coefficient, best_coefficients, validation_cycle.sigma_exponent)
+    if i % 1000 == 0:
+        print(best_rmse, best_nu_zero_coefficient, best_coefficients, best_sigma, flush = True)
 
 best_coefficients = torch.cat([best_coefficients, torch.ones((1,))], dim = -1)
 n_train = train_train_kernel.shape[0] 
 n_val = train_validation_kernel.shape[1]
 n_test = train_test_kernel.shape[1]
 
-
-alpha_grid = np.logspace(5, -10, 100)
-rmse = []
-for alpha in tqdm.tqdm(alpha_grid):
-    c = torch.linalg.solve(
-    best_nu_zero_coefficient * torch.ones((n_train, n_train)) +  # very dirty nu = 0 kernel
-    train_train_kernel @ best_coefficients +  # nu = 1, ..., 4 kernels
-    alpha * torch.eye(n_train)  # regularization
-    , 
-    train_energies)
-
-    validation_predictions = (
-        best_nu_zero_coefficient * torch.ones((n_val, n_train)) + 
-        (train_validation_kernel @ best_coefficients).T) @ c
-    rmse.append(get_rmse(validation_predictions, validation_energies).item())
-
-print(rmse)
-best_alpha = alpha_grid[np.argmin(rmse)]
-print(best_alpha, np.min(rmse))
-
-
-
 c = torch.linalg.solve(
     best_nu_zero_coefficient * torch.ones((n_train, n_train)) +  # very dirty nu = 0 kernel
     train_train_kernel @ best_coefficients +  # nu = 1, ..., 4 kernels
-    best_alpha * torch.eye(n_train)  # regularization
+    best_sigma * torch.eye(n_train)  # regularization
     , 
     train_energies)
 
@@ -312,6 +301,6 @@ test_predictions = (
     best_nu_zero_coefficient * torch.ones((n_test, n_train)) + 
     (train_test_kernel @ best_coefficients).T) @ c
 
-print("Test set RMSE (after kernel mixing): ", get_rmse(test_predictions, test_energies).item())
-
-
+print(f"Test set RMSE (after kernel mixing): {get_rmse(test_predictions, test_energies).item()}")
+print()
+print(n_train, get_rmse(test_predictions, test_energies).item())
